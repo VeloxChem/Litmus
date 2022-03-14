51f16c49c40bfc685ed33a052fd67b9121767269
Zilvinas Rinkevicius <rinkevic@kth.se>
2022-03-14 06:46:52 +0100

    Added Fraction class.

---
+++ src/algebra/Fraction.hpp
@@ -0,0 +1,104 @@
+// LITMUS: An Automated Molecular Integrals Generator
+// Copyright 2022 Z. Rinkevicius, KTH, Sweden.
+// E-mail: rinkevic@kth.se
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef Fraction_hpp
+#define Fraction_hpp
+
+#include <string>
+
+/// Fraction class.
+class Fraction
+{
+    /// Numerator of fraction.
+    int _numerator;
+    
+    /// Denominator of fraction.
+    int _denominator;
+    
+    /// Reduces numerator and denominator into standard form.
+    void _reduce();
+        
+public:
+    /// Creates an empy fraction.
+    Fraction();
+    
+    /// Creates a faction from only numerator.
+    /// @param numerator The numerator of fraction.
+    Fraction(const int numerator);
+    
+    /// Creates a faction from only nominator.
+    /// @param numerator The numerator of fraction.
+    /// @param denominator The nominator of fraction.
+    Fraction(const int numerator,
+             const int denominator);
+        
+    /// Compares this fraction with other fraction.
+    /// @param other The other fraction to compare.
+    /// @return true if fractions are equal, false otherwise.
+    bool operator==(const Fraction& other) const;
+    
+    /// Compares this fraction with other fraction.
+    /// @param other The other fraction to compare.
+    /// @return true if fractionss are not equal, false otherwise.
+    bool operator!=(const Fraction& other) const;
+    
+    /// Compares this fraction with other fraction.
+    /// @param other The other fraction to compare.
+    /// @return true if this fraction is less than other fraction, false otherwise.
+    bool operator<(const Fraction& other) const;
+    
+    /// Adds two fractions into single fraction.
+    /// @param other The other fraction to be added.
+    /// @return The fraction with two added fractions.
+    Fraction operator+(const Fraction& other) const;
+    
+    /// Substracts two fractions into single fraction.
+    /// @param other The other fraction to be added.
+    /// @return The fraction with two substracted fractions.
+    Fraction operator-(const Fraction& other) const;
+    
+    /// Multuplies two fractions into single fraction.
+    /// @param other The other fraction to be added.
+    /// @return The fraction with two multiplied fractions.
+    Fraction operator*(const Fraction& other) const;
+    
+    /// Devides two fractions into single fraction.
+    /// @param other The other fraction to be added.
+    /// @return The fraction with two divided fractions.
+    Fraction operator/(const Fraction& other) const;
+    
+    /// Returns numerator of fraction.
+    /// @return The numerator of fraction.
+    int numerator() const;
+    
+    /// Returns denominator of fraction.
+    /// @return The numerator of fraction.
+    int denominator() const;
+    
+    /// Determines if fraction is negative.
+    /// @return True if fraction is negative, false otherwise.
+    bool is_negative() const;
+    
+    /// Checks if fraction is not a number.
+    /// @return true if this fraction is not a number.
+    bool isNan() const;
+    
+    /// Creates primitive textual representation of this fraction.
+    /// @return The string with primitive textual representation of fraction.
+    std::string to_string() const;
+};
+
+#endif /* Fraction_hpp */
---
+++ tests/algebra/test_fraction.cpp
@@ -0,0 +1,162 @@
+// LITMUS: An Automated Molecular Integrals Generator
+// Copyright 2022 Z. Rinkevicius, KTH, Sweden.
+// E-mail: rinkevic@kth.se
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test_fraction.hpp"
+
+#include "fraction.hpp"
+
+TEST_F(FractionTest, Constructor)
+{
+    EXPECT_EQ(Fraction(), Fraction(0, 0));
+    
+    EXPECT_EQ(Fraction(2), Fraction(2, 1));
+    
+    EXPECT_EQ(Fraction(-2), Fraction(-2, 1));
+    
+    EXPECT_EQ(Fraction(2, -1), Fraction(-2, 1));
+    
+    EXPECT_EQ(Fraction(1, 2), Fraction(3, 6));
+}
+
+TEST_F(FractionTest, OperatorEqual)
+{
+    EXPECT_TRUE(Fraction(3) == Fraction(6, 2));
+}
+
+TEST_F(FractionTest, OperatorNotEqual)
+{
+    EXPECT_TRUE(Fraction(3) != Fraction(5, 2));
+}
+
+TEST_F(FractionTest, OperatorLess)
+{
+    EXPECT_FALSE(Fraction(1, 3) < Fraction(1, 4));
+    
+    EXPECT_TRUE(Fraction(1, 5) < Fraction(1, 4));
+}
+
+TEST_F(FractionTest, OperatorAdd)
+{
+    const auto lhsfrac = Fraction(2, 3) + Fraction(1, 5);
+    
+    const auto rhsfrac = Fraction(13, 15);
+    
+    EXPECT_EQ(lhsfrac, rhsfrac);
+}
+
+TEST_F(FractionTest, OperatorSubstract)
+{
+    const auto lhsfrac = Fraction(2, 3) - Fraction(1, 5);
+    
+    const auto rhsfrac = Fraction(7, 15);
+    
+    EXPECT_EQ(lhsfrac, rhsfrac);
+}
+
+TEST_F(FractionTest, OperatorMultiply)
+{
+    const auto lhsfrac = Fraction(2, 3) * Fraction(3, 5);
+    
+    const auto rhsfrac = Fraction(2, 5);
+    
+    EXPECT_EQ(lhsfrac, rhsfrac);
+}
+
+TEST_F(FractionTest, OperatorDivide)
+{
+    const auto lhsfrac = Fraction(2, 3) / Fraction(3, 5);
+    
+    const auto rhsfrac = Fraction(10, 9);
+    
+    EXPECT_EQ(lhsfrac, rhsfrac);
+}
+
+TEST_F(FractionTest, Numerator)
+{
+    auto frac = Fraction(-2, 3);
+    
+    EXPECT_EQ(frac.numerator(), -2);
+    
+    frac = Fraction(2, -3);
+    
+    EXPECT_EQ(frac.numerator(), -2);
+    
+    frac = Fraction(2, 3);
+    
+    EXPECT_EQ(frac.numerator(), 2);
+    
+    frac = Fraction(-2, -3);
+    
+    EXPECT_EQ(frac.numerator(), 2);
+}
+
+TEST_F(FractionTest, Denominator)
+{
+    auto frac = Fraction(-2, 3);
+    
+    EXPECT_EQ(frac.denominator(), 3);
+    
+    frac = Fraction(2, -3);
+    
+    EXPECT_EQ(frac.denominator(), 3);
+    
+    frac = Fraction(2, 3);
+    
+    EXPECT_EQ(frac.denominator(), 3);
+    
+    frac = Fraction(-2, -3);
+    
+    EXPECT_EQ(frac.denominator(), 3);
+}
+
+TEST_F(FractionTest, IsNegative)
+{
+    auto frac = Fraction(-2, 3);
+    
+    EXPECT_TRUE(frac.is_negative());
+    
+    frac = Fraction(2, -3);
+    
+    EXPECT_TRUE(frac.is_negative());
+    
+    frac = Fraction(2, 3);
+    
+    EXPECT_FALSE(frac.is_negative());
+    
+    frac = Fraction(-2, -3);
+    
+    EXPECT_FALSE(frac.is_negative());
+}
+
+TEST_F(FractionTest, ToString)
+{
+    auto frac = Fraction(-2, 3);
+    
+    EXPECT_EQ(frac.to_string(), "-2/3");
+    
+    frac = Fraction(2, -3);
+    
+    EXPECT_EQ(frac.to_string(), "-2/3");
+    
+    frac = Fraction(2, 3);
+    
+    EXPECT_EQ(frac.to_string(), "2/3");
+    
+    frac = Fraction(-2, -3);
+    
+    EXPECT_EQ(frac.to_string(), "2/3");
+}
+
