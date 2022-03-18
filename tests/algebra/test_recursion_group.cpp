af4b33bfdb068c93b2c26ebeb01d2db25cbddd1e
Zilvinas Rinkevicius <rinkevic@kth.se>
2022-03-18 08:49:37 +0100

    Added unit tests for RecursionGroup class.

--- tests/algebra/test_recursion_group.cpp
+++ tests/algebra/test_recursion_group.cpp
@@ -31,9 +31,11 @@ using R4CTerm = RecursionTerm<T4CIntegral>;
 
 using R4CDist = RecursionExpansion<T4CIntegral>;
 
+using R4Group = RecursionGroup<T4CIntegral>; 
+
 TEST_F(RecursionGroupTest, Constructor)
 {
-    EXPECT_EQ(R4CDist(), R4CDist(R4CTerm(), {}));
+    EXPECT_EQ(R4Group(), R4Group(VRecursionExpansions<T4CIntegral>({})));
     
     const auto s_0 = TensorComponent(0, 0, 0);
     
@@ -63,7 +65,15 @@ TEST_F(RecursionGroupTest, Constructor)
     
     const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(1, 3));
     
-    EXPECT_EQ(R4CDist(t4crt), R4CDist(t4crt, {}));
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    EXPECT_EQ(R4Group({t4crt, r4cdist}), R4Group({t4crt, r4cdist}));
 }
 
 TEST_F(RecursionGroupTest, OperatorBrackets)
@@ -102,7 +112,232 @@ TEST_F(RecursionGroupTest, OperatorBrackets)
     
     const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
     
-    EXPECT_EQ(t4cdist[0], r4crta);
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
     
-    EXPECT_EQ(t4cdist[1], r4crtb);
+    const auto t4group = R4Group({t4cdist, r4cdist});
+    
+    EXPECT_EQ(t4group[0], t4cdist);
+    
+    EXPECT_EQ(t4group[1], r4cdist);
+}
+
+TEST_F(RecursionGroupTest, OperatorEqual)
+{
+    const auto operi = OperatorComponent("1/|r-r'|");
+    
+    const auto p_x = TensorComponent(1, 0, 0);
+    
+    const auto p_y = TensorComponent(0, 1, 0);
+    
+    const auto opddr = OperatorComponent("d/dr", p_y, "bra", 1);
+    
+    const auto opddc = OperatorComponent("d/dC", p_x, "ket", 0);
+    
+    const auto s_0 = TensorComponent(0, 0, 0);
+    
+    const auto d_xy = TensorComponent(1, 1, 0);
+    
+    const auto f_yzz = TensorComponent(0, 1, 2);
+    
+    auto bpair = T2CPair({"GA", "GB"}, {p_x, f_yzz});
+    
+    auto kpair = T2CPair({"GC", "GD"}, {s_0, d_xy});
+
+    auto t4cint = T4CIntegral(bpair, kpair, operi, 2, {opddr, opddc});
+    
+    const auto pbx = Factor("(P-B)", "pb", p_x);
+    
+    const auto wpy = Factor("(W-P)", "wp", p_y);
+    
+    const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(3, 7));
+    
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    EXPECT_TRUE(R4Group({t4cdist, r4cdist}) == R4Group({t4cdist, r4cdist}));
+}
+
+TEST_F(RecursionGroupTest, OperatorNotEqual)
+{
+    const auto operi = OperatorComponent("1/|r-r'|");
+    
+    const auto p_x = TensorComponent(1, 0, 0);
+    
+    const auto p_y = TensorComponent(0, 1, 0);
+    
+    const auto opddr = OperatorComponent("d/dr", p_y, "bra", 1);
+    
+    const auto opddc = OperatorComponent("d/dC", p_x, "ket", 0);
+    
+    const auto s_0 = TensorComponent(0, 0, 0);
+    
+    const auto d_xy = TensorComponent(1, 1, 0);
+    
+    const auto f_yzz = TensorComponent(0, 1, 2);
+    
+    auto bpair = T2CPair({"GA", "GB"}, {p_x, f_yzz});
+    
+    auto kpair = T2CPair({"GC", "GD"}, {s_0, d_xy});
+
+    auto t4cint = T4CIntegral(bpair, kpair, operi, 2, {opddr, opddc});
+    
+    const auto pbx = Factor("(P-B)", "pb", p_x);
+    
+    const auto wpy = Factor("(W-P)", "wp", p_y);
+    
+    const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(3, 7));
+    
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    EXPECT_TRUE(R4Group({t4cdist, r4cdist}) != R4Group({r4cdist, r4cdist}));
+    
+    EXPECT_TRUE(R4Group({t4cdist, r4cdist}) != R4Group({t4cdist}));
+}
+
+TEST_F(RecursionGroupTest, OperatorLess)
+{
+    const auto operi = OperatorComponent("1/|r-r'|");
+    
+    const auto p_x = TensorComponent(1, 0, 0);
+    
+    const auto p_y = TensorComponent(0, 1, 0);
+    
+    const auto opddr = OperatorComponent("d/dr", p_y, "bra", 1);
+    
+    const auto opddc = OperatorComponent("d/dC", p_x, "ket", 0);
+    
+    const auto s_0 = TensorComponent(0, 0, 0);
+    
+    const auto d_xy = TensorComponent(1, 1, 0);
+    
+    const auto f_yzz = TensorComponent(0, 1, 2);
+    
+    auto bpair = T2CPair({"GA", "GB"}, {p_x, f_yzz});
+    
+    auto kpair = T2CPair({"GC", "GD"}, {s_0, d_xy});
+
+    auto t4cint = T4CIntegral(bpair, kpair, operi, 2, {opddr, opddc});
+    
+    const auto pbx = Factor("(P-B)", "pb", p_x);
+    
+    const auto wpy = Factor("(W-P)", "wp", p_y);
+    
+    const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(3, 7));
+    
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    EXPECT_TRUE(R4Group({t4cdist, r4cdist}) < R4Group({t4cdist, t4cdist}));
+    
+    EXPECT_FALSE(R4Group({t4cdist, r4cdist}) < R4Group({t4cdist}));
+}
+
+TEST_F(RecursionGroupTest, Add)
+{
+    const auto operi = OperatorComponent("1/|r-r'|");
+    
+    const auto p_x = TensorComponent(1, 0, 0);
+    
+    const auto p_y = TensorComponent(0, 1, 0);
+    
+    const auto opddr = OperatorComponent("d/dr", p_y, "bra", 1);
+    
+    const auto opddc = OperatorComponent("d/dC", p_x, "ket", 0);
+    
+    const auto s_0 = TensorComponent(0, 0, 0);
+    
+    const auto d_xy = TensorComponent(1, 1, 0);
+    
+    const auto f_yzz = TensorComponent(0, 1, 2);
+    
+    auto bpair = T2CPair({"GA", "GB"}, {p_x, f_yzz});
+    
+    auto kpair = T2CPair({"GC", "GD"}, {s_0, d_xy});
+
+    auto t4cint = T4CIntegral(bpair, kpair, operi, 2, {opddr, opddc});
+    
+    const auto pbx = Factor("(P-B)", "pb", p_x);
+    
+    const auto wpy = Factor("(W-P)", "wp", p_y);
+    
+    const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(3, 7));
+    
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    auto t4group = R4Group();
+    
+    EXPECT_EQ(t4group, R4Group(VRecursionExpansions<T4CIntegral>({})));
+ 
+    t4group.add(r4cdist);
+    
+    EXPECT_EQ(t4group, R4Group({r4cdist}));
+    
+    t4group.add(t4cdist);
+    
+    EXPECT_EQ(t4group, R4Group({r4cdist, t4cdist}));
+}
+
+TEST_F(RecursionGroupTest, Expansions)
+{
+    const auto operi = OperatorComponent("1/|r-r'|");
+    
+    const auto p_x = TensorComponent(1, 0, 0);
+    
+    const auto p_y = TensorComponent(0, 1, 0);
+    
+    const auto opddr = OperatorComponent("d/dr", p_y, "bra", 1);
+    
+    const auto opddc = OperatorComponent("d/dC", p_x, "ket", 0);
+    
+    const auto s_0 = TensorComponent(0, 0, 0);
+    
+    const auto d_xy = TensorComponent(1, 1, 0);
+    
+    const auto f_yzz = TensorComponent(0, 1, 2);
+    
+    auto bpair = T2CPair({"GA", "GB"}, {p_x, f_yzz});
+    
+    auto kpair = T2CPair({"GC", "GD"}, {s_0, d_xy});
+
+    auto t4cint = T4CIntegral(bpair, kpair, operi, 2, {opddr, opddc});
+    
+    const auto pbx = Factor("(P-B)", "pb", p_x);
+    
+    const auto wpy = Factor("(W-P)", "wp", p_y);
+    
+    const auto t4crt = R4CTerm(t4cint, {{pbx, 1}, {wpy, 2},}, Fraction(3, 7));
+    
+    const auto r4crta = R4CTerm(t4cint, {{pbx, 1},}, Fraction(1, 3));
+    
+    const auto r4crtb = R4CTerm(t4cint, {{wpy, 3},}, Fraction(1, 3));
+    
+    const auto t4cdist = R4CDist(t4crt, {r4crta, r4crtb});
+    
+    const auto r4cdist = R4CDist(r4crta, {r4crtb});
+    
+    const auto t4group = R4Group({t4cdist, r4cdist});
+    
+    EXPECT_EQ(t4group.expansions(), 2);
 }
