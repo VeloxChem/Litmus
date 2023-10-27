6096da16998f9f15b769ce61185512ccf2fb68b2
Zilvinas Rinkevicius <rinkevic@Zilvinass-iMac.lan>
2023-10-27 11:00:15 +0200

    * Adding four center geom. recursion.

---
+++ src/generators/t4c_center_driver.hpp
@@ -0,0 +1,61 @@
+#ifndef t4c_center_driver_hpp
+#define t4c_center_driver_hpp
+
+#include <optional>
+#include <array>
+
+#include "tensor_component.hpp"
+#include "t4c_defs.hpp"
+
+/// Four center geometrical prefix operator driver class.
+class T4CCenterDriver
+{
+    /// Cartesian coordinate tensor components.
+    std::array<TensorComponent, 3> _rxyz;
+    
+public:
+    /// Creates a two center geometrical prefix operator driver.
+    T4CCenterDriver();
+    
+    /// Check if recursion term is auxilary with respect of geometrical prefix operator.
+    /// @param rterm The recursion term.
+    /// @param index The index of prefix operator.
+    /// @return True if reccursion term is auxilary, False otherwise.
+    bool is_auxilary(const R4CTerm& rterm,
+                     const int      index) const;
+    
+    /// Applies vertical recursion to geometrical prefix operator acting bra or ket side of given recursion term.
+    /// @param rterm The recursion term.
+    /// @param axis The axis of vertical recursion.
+    /// @param index The index of prefix operator.
+    /// @return The recursion expansion of given recursion term.
+    std::optional<R4CDist> bra_ket_vrr(const R4CTerm& rterm,
+                                       const char     axis,
+                                       const int      index) const;
+    
+    /// Applies vertical recursion to geometrical prefix operator acting bra or ket side of given recursion term.
+    /// @param rterm The recursion term with overlap integral.
+    /// @param index The index of prefix operator.
+    /// @return The recursion expansion of given recursion term.
+    R4CDist apply_bra_ket_vrr(const R4CTerm& rterm,
+                              const int      index) const;
+    
+    
+    /// Recursively applies Obara-Saika recursion to recursion expansion.
+    /// @param rdist The recursion expansion.
+    void apply_recursion(R4CDist& rdist) const;
+    
+    /// Recursively applies vertical recursion to geometrical prefix operator acting bra or ket side of given recursion expansion.
+    /// @param rdist The recursion expansion.
+    /// @param index The index of prefix operator.
+    void apply_bra_ket_vrr(      R4CDist& rdist,
+                           const int      index) const;
+    
+    /// Creates recursion group from vector of integral components.
+    /// @param vints The  vector of integral components.
+    /// @return The recursion group.
+    R4Group create_recursion(const VT4CIntegrals& vints) const;
+};
+
+
+#endif /* t4c_center_driver_hpp */
